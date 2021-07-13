ae15847771485e411c33034e24ffb08b4bdb7d0d
Narendra Mannan <narenman7@gmail.com>
2021-07-13 21:46:19 +0000

    save user profile data to datastore

--- Roomz/src/main/java/com/google/sps/servlets/CreateProfileServlet.java
+++ Roomz/src/main/java/com/google/sps/servlets/CreateProfileServlet.java
@@ -19,19 +19,28 @@ public class CreateProfileServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
     // Sanitize user input to remove HTML tags and JavaScript.
-    String name = Jsoup.clean(request.getParameter("title"), Whitelist.none());
-    // long timestamp = System.currentTimeMillis();
+    String fullname = Jsoup.clean(request.getParameter("fullname"), Whitelist.none());
+    String email = Jsoup.clean(request.getParameter("email"), Whitelist.none());
+    String bio = Jsoup.clean(request.getParameter("bio"), Whitelist.none());
+    String age = Jsoup.clean(request.getParameter("age"), Whitelist.none());
+    String gender = Jsoup.clean(request.getParameter("gender"), Whitelist.none());
+
 
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     KeyFactory keyFactory = datastore.newKeyFactory().setKind("Users");
     FullEntity userEntity =
         Entity.newBuilder(keyFactory.newKey())
-            .set("name", name)
-            // .set("email", email)
+            .set("fullname", fullname)
+            .set("email", email)
+            .set("bio", bio)
+            .set("age", age)
+            .set("gender", gender)
             .build();
     datastore.put(userEntity);
 
-    response.sendRedirect("/index.html");
+    response.sendRedirect("/roommates.html");
+    
   }
 }
