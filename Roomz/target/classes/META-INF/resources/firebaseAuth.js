e4f92d7a380dbec7e8a2202452c9b29a69db0a42
Narendra Mannan <narenman7@gmail.com>
2021-07-21 17:52:27 +0000

    code clean up, nav bar and logout bug fixes for index,profile and edit profile pages

---
+++ Roomz/target/classes/META-INF/resources/firebaseAuth.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        var firebaseConfig = {
+            apiKey: "AIzaSyC4El1FbprSPpcq0FlRxzm79mE6vyU2dEY",
+            authDomain: "summer21-sps-7.firebaseapp.com",
+            projectId: "summer21-sps-7",
+            storageBucket: "summer21-sps-7.appspot.com",
+            messagingSenderId: "1046630479691",
+            appId: "1:1046630479691:web:596d4dbc7586b5152f9ace",
+            measurementId: "G-GETT4XG4HE"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
